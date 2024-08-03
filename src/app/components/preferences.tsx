d211894e0663802b565268edd3d5ae677bcf8528
john-roufaeil <john.flouis1@gmail.com>
2024-08-03 17:38:46 +0300

    feat: first steps in header and preferences

---
+++ src/app/components/preferences.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import React, { useState } from 'react';
+import {
+    MoonIcon,
+    SunIcon,
+    LanguageIcon,
+    MagnifyingGlassIcon,
+} from '@heroicons/react/24/outline';
+
+export default function Preferences() {
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    let changeTheme = () => {
+        setIsDarkMode(prev => {
+            document.body.classList.toggle('dark', !prev);
+            return !prev;
+        });
+    }
+
+    return (
+        <aside className="bg-light-sub-bg dark:bg-dark-sub-bg text-light-text dark:text-dark-text fixed right-0 top-1/2 w-8 h-fit rounded-l-lg">
+            <ul className='flex flex-col justify-between gap-2 p-2'>
+                <li>
+                    <button id="change-theme" onClick={changeTheme} className='w-full h-full hover:text-light-accent dark:hover:text-dark-accent'>
+                        {isDarkMode ? <SunIcon /> : <MoonIcon />}
+                    </button></li>
+                <li><button id="change-language" className='w-full h-full hover:text-light-accent dark:hover:text-dark-accent'><LanguageIcon></LanguageIcon></button></li>
+                <li><button id="change-size" className='w-full h-full hover:text-light-accent dark:hover:text-dark-accent'><MagnifyingGlassIcon></MagnifyingGlassIcon></button></li>
+            </ul >
+        </aside >
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,20 @@
 import Image from "next/image";
+import Header from "./components/header"
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-light-bg dark:bg-dark-bg contrast:bg-contrast-bg">
-            <div className="card bg-light-sub-bg dark:bg-dark-sub-bg p-5 rounded-lg">
-                <h1 className="text-light-primary dark:text-dark-primary text-5xl">IEEE GUC</h1>
-                <p className="text-light-secondary dark:text-dark-secondary">Welcome</p>
-                <p className="text-light-accent dark:text-dark-accent">Welcome</p>
-                <p className="text-light-text dark:text-dark-text">Welcome</p>
-            </div>
-        </main>
+        <>
+            <Header />
+            <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-light-bg dark:bg-dark-bg contrast:bg-contrast-bg">
+                <h1 className="text-5xl text-light-text dark:text-dark-text h-fit"> IEEE GUC Student Branch </h1>
+                {/* <div className="card bg-light-sub-bg dark:bg-dark-sub-bg p-5 rounded-lg">
+                    <h1 className="text-light-primary dark:text-dark-primary text-5xl">IEEE GUC</h1>
+                    <p className="text-light-secondary dark:text-dark-secondary">Welcome</p>
+                    <p className="text-light-accent dark:text-dark-accent">Welcome</p>
+                    <p className="text-light-text dark:text-dark-text">Welcome</p>
+                </div> */}
+                <div className="w-7/12 bg-light-sub-bg dark:bg-dark-sub-bg h-28"></div>
+            </main>
+        </>
     );
 }
