7053898ce05448b70e05d39b6d0d2ecf39aa34dd
john-roufaeil <john.flouis1@gmail.com>
2024-08-29 22:50:04 +0300

    feat: add dashboards

---
+++ src/app/(routes)/member/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import ComingSoon from "../../../components/ComingSoon"
+
+export default function Dashboard() {
+    return (
+        <main className="flex w-full min-h-screen flex-col items-center justify-between py-12 p-6 bg-light-bg dark:bg-dark-bg contrast:bg-contrast-bg">
+            <section className="about sm:w-8/12 w-full">
+                <h1 className="text-5xl text-light-text dark:text-dark-text h-fit"> Member Dashboard</h1>
+                <div className="typewriter"><p className="text-light-text dark:text-dark-text dark:dark p-1">Team Work Makes The Dream Work</p></div>
+                <ComingSoon />
+            </section>
+        </main >
+    )
+}
