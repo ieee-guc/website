3ab55f384e3def14f13616d0f60d5cba80deff49
john-roufaeil <john.flouis1@gmail.com>
2024-08-30 04:47:14 +0300

    feat: sidebar animate open and close desktop

--- src/app/components/SideBar.tsx
+++ src/app/components/SideBar.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { useState } from 'react';
-import { ArrowRight } from 'react-feather';
+import { ChevronRight, ChevronLeft } from 'react-feather';
 import Logo from '../../../public/ieee-logo.png'
 import Image from 'next/image';
 
@@ -8,53 +8,68 @@ export default function SideBar() {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <>
-            <div className={`flex flex-col items-center ${isOpen ? 'w-40' : 'w-16'} overflow-hidden text-light-text dark:text-dark-text bg-light-sub-bg dark:bg-dark-sub-bg rounded-r-xl h-dvh fixed group`}>
-                <a className="flex items-center w-full px-3 mt-3" href="#">
+        <div className="relative">
+            {!isOpen && <button
+                className={`absolute top-4 left-10 z-10 bg-light-sub-bg dark:bg-dark-sub-bg flex items-center w-10 h-10 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-light-border ${isOpen ? 'bg-light-border' : 'bg-light-sub-bg'} text-white transition duration-200 ease-in-out align-middle justify-end`}
+                onClick={() => setIsOpen(!isOpen)}
+            >
+                <ChevronRight />
+            </button>}
+            <div className={`flex flex-col items-center z-50 ${isOpen ? 'w-44' : 'w-16'} transition-all duration-300 text-light-text dark:text-dark-text bg-light-sub-bg dark:bg-dark-sub-bg rounded-r-xl h-dvh fixed`}>
+                <button onClick={() => setIsOpen(!isOpen)}
+                    className="flex items-center w-full px-3 mt-3">
                     <Image src={Logo} width={40} height={40} alt="IEEE" className="rounded-xl" />
                     {isOpen && <span className="ml-2 text-sm font-bold ">IEEE GUC</span>}
-                </a>
+
+                    {isOpen && <button
+                        className={`z-10 bg-light-sub-bg dark:bg-dark-sub-bg flex items-center w-10 h-10 rounded-r-full ${isOpen ? 'bg-light-border' : 'bg-light-sub-bg'} text-white transition duration-200 ease-in-out align-middle justify-center`}
+                        onClick={() => setIsOpen(!isOpen)}
+                    >
+                        <ChevronLeft />
+                    </button>}
+                </button>
                 <div className="w-full px-2">
-                    <div className="flex flex-col items-center w-full mt-3 border-t border-gray-700">
-                        <a className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300" href="#">
+                    <div className="flex flex-col items-center w-full mt-3 border-t border-light-border dark:border-dark-border align-middle">
+                        <a className="relative group flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-light-border hover:text-gray-300" href="/director/dashboard">
                             <svg className="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
                             </svg>
-                            {isOpen && <span className="ml-2 text-sm font-medium ">Dasboard</span>}
+                            {!isOpen && <span className=" absolute left-0 hidden bg-light-border group-hover:left-10 h-12 p-4 transition-all duration-300 rounded z-0">Dashboard</span>}
+                            {isOpen && <span className="ml-2 text-sm font-medium ">Dashboard</span>}
                         </a>
-                        <a className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300" href="#">
+                        <a className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-light-border hover:text-gray-300" href="#">
                             <svg className="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                             </svg>
                             {isOpen && <span className="ml-2 text-sm font-medium ">Search</span>}
                         </a>
-                        <a className="flex items-center w-full h-12 px-3 mt-2 text-gray-200 bg-gray-700 rounded" href="#">
+                        <a className="flex items-center w-full h-12 px-3 mt-2 text-gray-200 bg-light-border rounded" href="#">
                             <svg className="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 8v8m-4-5v5m-4-2v2m-2 4h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
                             </svg>
                             {isOpen && <span className="ml-2 text-sm font-medium ">Insights</span>}
                         </a>
-                        <a className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300" href="#">
+                        <a className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-light-border hover:text-gray-300" href="#">
                             <svg className="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7v8a2 2 0 002 2h6M8 7V5a2 2 0 012-2h4.586a1 1 0 01.707.293l4.414 4.414a1 1 0 01.293.707V15a2 2 0 01-2 2h-2M8 7H6a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2v-2" />
                             </svg>
                             {isOpen && <span className="ml-2 text-sm font-medium ">Docs</span>}
                         </a>
                     </div>
-                    <div className="flex flex-col items-center w-full mt-2 border-t border-gray-700">
-                        <a className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300" href="#">
+                    <div className="flex flex-col items-center w-full mt-2 border-t border-light-border dark:border-dark-border">
+                        <a className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-light-border hover:text-gray-300" href="#">
                             <svg className="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
                             </svg>
                             {isOpen && <span className="ml-2 text-sm font-medium ">Products</span>}
                         </a>
-                        <a className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300" href="#">
+                        <a className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-light-border hover:text-gray-300" href="#">
                             <svg className="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4" />
                             </svg>
                             {isOpen && <span className="ml-2 text-sm font-medium ">Settings</span>}
                         </a>
-                        <a className="relative flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300" href="#">
+                        <a className="relative flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-light-border hover:text-gray-300" href="#">
                             <svg className="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
                             </svg>
@@ -63,7 +78,7 @@ export default function SideBar() {
                         </a>
                     </div>
                 </div>
-                <a className="flex items-center justify-center w-full h-16 mt-auto bg-gray-800 hover:bg-gray-700 hover:text-gray-300" href="#">
+                <a className="flex items-center justify-center w-full h-16 mt-auto bg-gray-800 hover:bg-light-border hover:text-gray-300" href="#">
                     <svg className="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
@@ -71,6 +86,6 @@ export default function SideBar() {
                 </a>
             </div>
 
-        </>
+        </div>
     )
 }
