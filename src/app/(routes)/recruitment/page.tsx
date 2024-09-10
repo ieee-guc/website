6d1cffd374d9ea68b7667c0b90c1d5d81eef6906
john-roufaeil <john.flouis1@gmail.com>
2024-09-10 22:35:27 +0300

    feat: make recruitment text conciser

--- src/app/(routes)/recruitment/page.tsx
+++ src/app/(routes)/recruitment/page.tsx
@@ -12,6 +12,7 @@ import {
     AccordionTrigger,
 } from "@/components/ui/accordion"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { ChevronsRight } from "react-feather";
 
 export default function Recruitment() {
     const [committees, setCommittees] = useState<Committee[]>([]);
@@ -53,39 +54,37 @@ export default function Recruitment() {
     };
 
     return (
-        <main className="flex w-full min-h-screen flex-col items-center justify-between py-4 p-6 bg-light-bg dark:bg-dark-bg">
+        <main className="flex w-full min-h-screen flex-col items-center justify-between py-4 p-2 bg-light-bg dark:bg-dark-bg">
             <section className="about sm:w-8/12 w-11/12">
-                {/* <h1 className="text-5xl text-light-text dark:text-dark-text h-fit"> Recruitment</h1>
-                <div className="typewriter"><p className="text-light-text dark:text-dark-text dark:dark p-1">Join Us Today!</p></div> */}
-                <div className="flex flex-col items-center  p-4 w-full  h-full sm:py-16 py-8 rounded-xl ">
-                    <p className="text-light-text dark:text-dark-text text-center text-2xl">
+                <div className="flex flex-col items-center  p-2 w-full  h-full sm:py-16 py-8 rounded-xl ">
+                    <p className="text-light-text dark:text-dark-text text-center text-xl">
                         <span className='font-bold text-3xl'>IEEE GUC Recruitment is now open! 🚀</span>
-
                         <br />
                         <br />
-                        Looking forward to spice up your academic year with fun and self-improvement?
+                        Ready for a fun, growth-filled year?
                         <br />
-                        You&apos;ve come to the right place! Hurry up and secure your place in one of our amazing committees.
+                        <span className="italic text-light-red dark:text-dark-red font-semibold">Secure your spot before 21/9!</span>
                         <br />
-                        <br />
-                        <span className="italic text-light-red dark:text-dark-red font-semibold">Recruitment closes on 21/9!</span>
-                        <br />
-                        We look forward to have you in our family! 😊
+                        We can&apos;t wait to welcome you! 😊
                     </p>
                     <Link
                         href="/recruitment/apply"
                         target="_blank"
-                        className="mt-4 text-2xl p-6 bg-light-primary dark:bg-dark-primary text-dark-text rounded-xl hover:scale-105 active:scale-95 transform transition-all duration-300"
+                        className="overflow-hidden mt-2 signin-button relative sm:w-1/4 w-3/4 text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-lg items-center px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 bg-light-primary flex justify-center"
                     >
-                        Join Us Now!
+                        <p>Join Us Now!</p>
+                        <ChevronsRight
+                            className="feather-chevron-right text-white"
+                            size={24}
+                        />
                     </Link>
                 </div>
 
-                <div id="committees" className="flex flex-col items-center mt-8 p-4 w-full shadow bg-light-sub-bg dark:bg-dark-sub-bg h-full py-16 rounded-xl border-light-border dark:border">
+                <div id="committees" className="flex flex-col items-center mt-2 p-2 w-full shadow bg-light-sub-bg dark:bg-dark-sub-bg h-full py-12 rounded-xl border-light-border dark:border">
                     <p className="text-light-text dark:text-dark-text text-center">
                         <span className='font-bold text-3xl'>Our Committees ⭐</span>
                     </p>
-                    <Accordion type="single" collapsible className="w-full sm:w-3/4 bg-white px-4 rounded-xl mt-4">
+                    <Accordion type="single" collapsible className="w-full sm:w-3/4 bg-white px-4 rounded-xl mt-2">
                         {uniqueDirectories.map((directory, index) => (
                             <AccordionItem key={index} value={`item-${index}`}>
                                 <AccordionTrigger className="text-xl font-semibold">
@@ -103,12 +102,11 @@ export default function Recruitment() {
                                                         src={committee.photoURL}
                                                         alt={committee.name}
                                                         className="w-full h-full object-cover rounded-lg"
-                                                        // fill={true}
                                                         width={200}
                                                         height={200}
                                                     />
                                                 </div>
-                                                <div className="txtContainer flex-col sm:w-3/4">
+                                                <div className="txtContainer flex-col sm:w-3/4 space-y-4 sm:space-y-2">
                                                     <div>
                                                         <div className="flex items-baseline gap-2">
                                                             <p className="text-xl font-semibold">{committee.icon} </p>
