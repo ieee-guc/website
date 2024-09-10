08fbeaf0c2e1530731f1f03c3d5a02cafbabc6d5
john-roufaeil <john.flouis1@gmail.com>
2024-09-10 19:05:09 +0300

    feat: install shadcn

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,36 +6,79 @@ const config: Config = {
         "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     ],
-    darkMode: "class",
+    darkMode: ["class", "class"],
     theme: {
-        extend: {
-            colors: {
-                light: {
-                    primary: '#0A4593',
-                    secondary: '#6EACDA',
-                    accent: '#ea580c',
-                    text: '#021526',
-                    bg: '#f9fafb',
-                    border: '#e2e8f0',
-                    // 'sub-bg': '#e2e8f0',
-                    'sub-bg': '#f3f4f6',
-                    'nav-bg': '#ffffff',
-                    'red': '#b91c1c',
-                },
-                dark: {
-                    primary: '#0A4593',
-                    secondary: '#009DDC',
-                    accent: '#fcd34d',
-                    text: '#EFF8E2',
-                    bg: '#0f172a',
-                    border: '#374151',
-                    'sub-bg': '#1f2937',
-                    // 'sub-bg': '#1e293b',
-                    'nav-bg': '#020617',
-                    'red': '#f87171',
-                },
-            },
-        },
+    	extend: {
+    		colors: {
+    			light: {
+    				primary: '#0A4593',
+    				secondary: '#6EACDA',
+    				accent: '#ea580c',
+    				text: '#021526',
+    				bg: '#f9fafb',
+    				border: '#e2e8f0',
+    				'sub-bg': '#f3f4f6',
+    				'nav-bg': '#ffffff',
+    				red: '#b91c1c'
+    			},
+    			dark: {
+    				primary: '#0A4593',
+    				secondary: '#009DDC',
+    				accent: '#fcd34d',
+    				text: '#EFF8E2',
+    				bg: '#0f172a',
+    				border: '#374151',
+    				'sub-bg': '#1f2937',
+    				'nav-bg': '#020617',
+    				red: '#f87171'
+    			},
+    			background: 'hsl(var(--background))',
+    			foreground: 'hsl(var(--foreground))',
+    			card: {
+    				DEFAULT: 'hsl(var(--card))',
+    				foreground: 'hsl(var(--card-foreground))'
+    			},
+    			popover: {
+    				DEFAULT: 'hsl(var(--popover))',
+    				foreground: 'hsl(var(--popover-foreground))'
+    			},
+    			primary: {
+    				DEFAULT: 'hsl(var(--primary))',
+    				foreground: 'hsl(var(--primary-foreground))'
+    			},
+    			secondary: {
+    				DEFAULT: 'hsl(var(--secondary))',
+    				foreground: 'hsl(var(--secondary-foreground))'
+    			},
+    			muted: {
+    				DEFAULT: 'hsl(var(--muted))',
+    				foreground: 'hsl(var(--muted-foreground))'
+    			},
+    			accent: {
+    				DEFAULT: 'hsl(var(--accent))',
+    				foreground: 'hsl(var(--accent-foreground))'
+    			},
+    			destructive: {
+    				DEFAULT: 'hsl(var(--destructive))',
+    				foreground: 'hsl(var(--destructive-foreground))'
+    			},
+    			border: 'hsl(var(--border))',
+    			input: 'hsl(var(--input))',
+    			ring: 'hsl(var(--ring))',
+    			chart: {
+    				'1': 'hsl(var(--chart-1))',
+    				'2': 'hsl(var(--chart-2))',
+    				'3': 'hsl(var(--chart-3))',
+    				'4': 'hsl(var(--chart-4))',
+    				'5': 'hsl(var(--chart-5))'
+    			}
+    		},
+    		borderRadius: {
+    			lg: 'var(--radius)',
+    			md: 'calc(var(--radius) - 2px)',
+    			sm: 'calc(var(--radius) - 4px)'
+    		}
+    	}
     },
     variants: {
         extend: {
@@ -44,6 +87,6 @@ const config: Config = {
             borderColor: ['dark', 'high-contrast'],
         },
     },
-    plugins: [],
+    plugins: [require("tailwindcss-animate")],
 };
 export default config;
