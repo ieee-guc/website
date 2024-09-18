8a72b9828e26dccfc5dfd4c6b66e630b9040fb41
john-roufaeil <john.flouis1@gmail.com>
2024-09-18 12:06:16 +0300

    feat: back to ommittees

--- src/app/(routes)/recruitment/apply/page.tsx
+++ src/app/(routes)/recruitment/apply/page.tsx
@@ -307,7 +307,7 @@ export default function RecruitmentForm() {
                                                 </button>
                                             )}
                                             <p className="text-xs font-light text-gray-500 dark:text-gray-400">
-                                                Already applied? <Link href="/login" className="font-medium hover:underline">Check application status</Link>
+                                                <Link href="/login" className="font-medium hover:underline">Back to committees</Link>
                                             </p>
                                         </Form>
                                     )}
