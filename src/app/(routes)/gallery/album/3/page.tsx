e918d233dfd23063c453513783f330154a8af1bc
john-roufaeil <john.flouis1@gmail.com>
2025-01-22 13:46:46 +0200

    feat: gallery grid view

---
+++ src/app/(routes)/gallery/album/3/page.tsx
@@ -0,0 +1,51 @@
+import type { Metadata } from "next";
+import { Card, CardContent } from "@/components/ui/card";
+import Image from "next/image";
+import Link from "next/link";
+import fs from "fs";
+import path from "path";
+import { ArrowLeft } from "react-feather";
+
+export const metadata: Metadata = {
+    title: "Gallery",
+};
+
+export default async function Album3() {
+    const imageDirectory = path.join(process.cwd(), "public/gallery/album3");
+    const images = fs.readdirSync(imageDirectory).map((fileName) => ({
+        src: `/gallery/album3/${fileName}`,
+    }));
+    return (
+        <main className="flex w-full min-h-screen flex-col items-center justify-between py-12 p-6 bg-light-bg dark:bg-dark-bg contrast:bg-contrast-bg">
+            <section className="about sm:w-8/12 w-full">
+                <div className="inline-flex items-start space-x-2">
+                    <Link rel="noopener noreferrer" href={'/gallery'}>
+                        <ArrowLeft size={36} className="mt-1.5 text-4xl hover:text-light-primary dark:hover:text-dark-secondary duration-300 text-light-text dark:text-dark-text" />
+                    </Link>
+                    <div>
+                        <h1 className="text-4xl sm:text-5xl text-light-text dark:text-dark-text h-fit"> PADELETO - PADEL TOURNAMENT </h1>
+                        <h1 className="text-base sm:text-xl text-light-text dark:text-dark-text h-fit underline"> NOVEMBER 2024</h1>
+                    </div>
+                </div>
+                <div className="grid grid-cols-1 lg:grid-cols-3 my-10">
+                    {images.map((item, index) => (
+                        <Card key={index} className="w-full p-0 h-72">
+                            <CardContent className="group hover:cursor-pointer flex items-center justify-center p-0 w-full h-full flex-col">
+                                <div className="relative overflow-hidden w-full h-full">
+                                    <Image
+                                        alt="image"
+                                        src={item.src}
+                                        layout="fill"
+                                        className="w-full h-full group-hover:scale-110 transition-all duration-300 object-cover"
+                                        placeholder="blur"
+                                        blurDataURL="/logo-mini.png"
+                                    />
+                                </div>
+                            </CardContent>
+                        </Card>
+                    ))}
+                </div>
+            </section>
+        </main >
+    )
+}
--- src/app/(routes)/gallery/page.tsx
+++ src/app/(routes)/gallery/page.tsx
@@ -2,20 +2,21 @@ import ComingSoon from "../../components/ComingSoon"
 import type { Metadata } from "next";
 import { Card, CardContent } from "@/components/ui/card";
 import Image from "next/image";
-import openingImg from "../../../../public/gallery/opening24/cover.png";
-import padeletoImg from "../../../../public/gallery/padeleto24/cover.png";
-import alexImg from "../../../../public/gallery/alex24/cover.png";
-import triviaImg from "../../../../public/gallery/trivia24/cover.jpg";
+import openingImg from "../../../../public/gallery/album1/cover.png";
+import padeletoImg from "../../../../public/gallery/album3/cover.png";
+import alexImg from "../../../../public/gallery/album2/cover.png";
+import triviaImg from "../../../../public/gallery/album4/cover.jpg";
+import Link from "next/link";
 
 export const metadata: Metadata = {
-    title: "IEEE GUC - Gallery",
+    title: "Gallery",
 };
 export default function Gallery() {
     const galleryItems = [
-        { photo: triviaImg, title: "TRIVIA EVENT", date: "DECEMBER 2024" },
-        { photo: padeletoImg, title: "PADELETO - PADEL TOURNAMENT", date: "NOVEMBER 2024" },
-        { photo: alexImg, title: "ALEX TRIP", date: "NOVEMBER 2024" },
-        { photo: openingImg, title: "OPENING", date: "OCTOBER 2024" },
+        { index: 4, photo: triviaImg, title: "TRIVIA EVENT", date: "DECEMBER 2024" },
+        { index: 3, photo: padeletoImg, title: "PADELETO - PADEL TOURNAMENT", date: "NOVEMBER 2024" },
+        { index: 2, photo: alexImg, title: "ALEX TRIP", date: "NOVEMBER 2024" },
+        { index: 1, photo: openingImg, title: "OPENING", date: "OCTOBER 2024" },
     ]
     return (
         <main className="flex w-full min-h-screen flex-col items-center justify-between py-12 p-6 bg-light-bg dark:bg-dark-bg contrast:bg-contrast-bg">
@@ -23,16 +24,22 @@ export default function Gallery() {
                 <h1 className="text-5xl text-light-text dark:text-dark-text h-fit"> Gallery</h1>
                 <div className="typewriter"><p className="text-light-text dark:text-dark-text dark:dark p-1">Team Work Makes The Dream Work</p></div>
                 <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 my-10">
-                    {galleryItems.map((item, index) => (
-                        <Card key={index} className="w-full rounded-xl p-0 h-72">
-                            <CardContent className="group hover:cursor-pointer flex items-center justify-center p-0 w-full h-full flex-col">
-                                <div className="relative overflow-hidden rounded-t-xl w-full h-full">
-                                    <Image src={item.photo} alt={item.title} layout="fill" className="w-full h-full rounded-t-xl group-hover:scale-110 transition-all duration-300 object-cover" />
-                                </div>
-                                <p className="group-hover:text-lg rounded-b-xl font-bold w-full text-center opacity-75 group-hover:opacity-100 transition-all duration-300 p-2 bg-light-nav-bg dark:bg-dark-nav-bg text-light-text dark:text-dark-text">{item.title}</p>
-                                <p className="group-hover:text-sm rounded-b-xl pb-2 font-bold text-xs w-full text-center opacity-75 group-hover:opacity-100 transition-all duration-300 bg-light-nav-bg dark:bg-dark-nav-bg text-light-text dark:text-dark-text">{item.date}</p>
-                            </CardContent>
-                        </Card>
+                    {galleryItems.map(item => (
+                        <Link key={item.index}
+                            rel="noopener noreferrer"
+                            href={'gallery/album/' + item.index}
+                            className="text-lg p-1.5 underline-offset-4 hover:text-light-primary hover:dark:text-dark-secondary hover:font-bold text-center text-light-text dark:text-dark-text"
+                        >
+                            <Card className="w-full rounded-xl p-0 h-72">
+                                <CardContent className="group hover:cursor-pointer flex items-center justify-center p-0 w-full h-full flex-col">
+                                    <div className="relative overflow-hidden rounded-t-xl w-full h-full">
+                                        <Image src={item.photo} alt={item.title} layout="fill" className="w-full h-full rounded-t-xl group-hover:scale-110 transition-all duration-300 object-cover" />
+                                    </div>
+                                    <p className="group-hover:text-lg text-base rounded-b-xl font-bold w-full text-center opacity-75 group-hover:opacity-100 transition-all duration-300 p-2 bg-light-nav-bg dark:bg-dark-nav-bg text-light-text dark:text-dark-text">{item.title}</p>
+                                    <p className="group-hover:text-sm rounded-b-xl pb-2 font-bold text-xs w-full text-center opacity-75 group-hover:opacity-100 transition-all duration-300 bg-light-nav-bg dark:bg-dark-nav-bg text-light-text dark:text-dark-text">{item.date}</p>
+                                </CardContent>
+                            </Card>
+                        </Link>
                     ))}
                 </div>
             </section>
