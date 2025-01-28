d0aeb144b14101fbf56280be7e20b44a4aed7a4c
john-roufaeil <john.flouis1@gmail.com>
2025-01-28 23:52:18 +0200

    feat: add description to the website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export const inter = Inter({
 })
 export const metadata: Metadata = {
     title: "IEEE GUC",
-    description: "IEEE GUC",
+    description: "IEEE GUC is a the most active technical club at the German University in Cairo. We are a dynamic community of students from various fields, united by a passion for technology, innovation, and professional growth. As part of the world’s largest technical professional organization, our club provides a platform for members to enhance their technical knowledge, develop leadership skills, and engage in hands-on projects.",
 };
 
 export default function RootLayout({
